1cfbeb2ce933e9970dfa2344bc9b8ca511cb248c
KZIX-MON <61951573+KZIX-MON@users.noreply.github.com>
2020-03-10 22:17:58 -0400

    Add files via upload

---
+++ arch-install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+read -p "Which partion do you want to use? " sdx
+
+timedatectl set-ntp true
+
+umount -R /mnt || echo "nothing to unmount" 
+
+wipefs -a /dev/"$sdx"
+
+echo "Numbers only [Max size is 512M]"
+echo "In Megabytes:"
+read -p "Boot Partition Size: " boot
+echo "Numbers only"
+echo "In Gigabytes"
+read -p "Root Partition Size: " root
+echo "Numbers Only"
+echo "In Gigabytes"
+read -p "Home Partition Size: " home
+
+cat <<EOF | fdisk /dev/"$sdx"
+o
+n
+p
+
+
++${boot}M
+n
+p
+
+
++${root}G
+n
+p
+
+
++${home}G
+n
+p
+
+
+w
+EOF
+
+
+yes | mkfs.ext4 /dev/"$sdx"1
+yes | mkfs.ext4 /dev/"$sdx"2
+yes | mkfs.ext4 /dev/"$sdx"3
+
+mount /dev/"$sdx"2 /mnt
+mkdir -p /mnt/home
+mount /dev/"$sdx"3 /mnt/home
+mkdir -p /mnt/boot
+mount /dev/"$sdx"1 /mnt/boot
+
+pacstrap /mnt base base-devel linux linux-firmware
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp chroot.sh /mnt/root
+
+arch-chroot /mnt
+
+
---
+++ chroot.sh
@@ -0,0 +1,32 @@
+#Chroot
+
+echo "root password:"
+passwd
+
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+#For the Lang Variable
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+#for the fonts
+echo "en_US.UTF-8 UTF-8" >> /etc/loacle.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+#Network Manager
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+#Grub
+pacman -S --noconfirm grub
+grub-install --target=i386-pc /dev/sdb 
+grub-mkconfig -o /boot/grub/grub.cfg
+
+read -p "Enter your username: " $user
+
+useradd -a -m -G audio,optical,storage,video,wheel $user
+
+echo "$user password:"
+
+passwd $user
